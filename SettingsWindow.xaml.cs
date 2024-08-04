0f928e36e2e273d625fa50e66fbf02d4e817e2a2
will181 <willmoffat181@gmail.com>
2024-08-04 22:37:51 +0100

    Adding support for settings background directory in settings (currently unused)

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -7,9 +7,20 @@ namespace MoffBackgroundSwitcher
     /// </summary>
     public partial class SettingsWindow : Window
     {
+        private readonly Settings _settings;
+
         public SettingsWindow(Settings settings)
         {
             InitializeComponent();
+            _settings = settings;
+
+            BackgroundDirectoryInput.Text = settings.BackgroundDirectory;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _settings.BackgroundDirectory = BackgroundDirectoryInput.Text;
+            _settings.Save();
         }
     }
 }
